5e6757fc551d80da486d1fec4eb718dab05a85e6
Ravi Prakash Singh <ssingh.raviprakash@gmail.com>
2022-07-08 12:53:39 +0530

    Completed

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -43,6 +43,8 @@ public class AppController {
 	public String listUsers(Model model) {
 		List<User> listUsers = userRepo.findAll();
 		model.addAttribute("listUsers", listUsers);
+		// retun no of users
+		model.addAttribute("noOfUsers", listUsers.size());
 
 		return "users";
 	}
@@ -84,6 +86,12 @@ public class AppController {
 		}
 		List<User> listUsers = userRepo.customQuery(name, age, profession, email, mobileNumber);
 		model.addAttribute("listUsers", listUsers);
+		if (listUsers.isEmpty()) {
+			model.addAttribute("message", "No results found");
+		} else {
+			// n results found
+			model.addAttribute("message", listUsers.size() + " results found");
+		}
 
 		return "query";
 	}
